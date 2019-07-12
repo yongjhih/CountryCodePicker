1ed8d5349bc943c621c67ca552726a0c670d044a
Andrew Chen <yongjhih@gmail.com>
2019-07-12 13:09:44 -0700

    Optimize with ListView.builder

--- lib/selection_dialog.dart
+++ lib/selection_dialog.dart
@@ -18,45 +18,88 @@ class SelectionDialog extends StatefulWidget {
 class _SelectionDialogState extends State<SelectionDialog> {
   /// this is useful for filtering purpose
   List<CountryCode> showedElements = [];
+  List<Widget> widgets = [];
 
   @override
-  Widget build(BuildContext context) => new SimpleDialog(
-      title: new Column(
+  void initState() {
+    super.initState();
+    /// Optimize for ViewModels
+    /// Using ListView.buidler is way better performance instaed
+    /*
+    widgets = <Widget>[]
+      ..addAll(widget.favoriteElements
+          .map(
+            (f) => new SimpleDialogOption(
+          child: _buildOption(f),
+          onPressed: () {
+            _selectItem(f);
+          },
+        ),
+      )
+          .toList())
+      ..add(new Divider())
+      ..addAll(showedElements
+          .map(
+            (e) => new SimpleDialogOption(
+          key: Key(e.toLongString()),
+          child: _buildOption(e),
+          onPressed: () {
+            _selectItem(e);
+          },
+        ),
+      )
+      .toList());
+    */
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Column(
         children: <Widget>[
-          new TextField(
-            decoration: new InputDecoration(prefixIcon: new Icon(Icons.search)),
+          TextField(
+            decoration: InputDecoration(prefixIcon: Icon(Icons.search)),
             onChanged: _filterElements,
           ),
         ],
       ),
-      children: [
-        widget.favoriteElements.isEmpty
-            ? new Container()
-            : new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[]
-                  ..addAll(widget.favoriteElements
-                      .map(
-                        (f) => new SimpleDialogOption(
-                              child: _buildOption(f),
-                              onPressed: () {
-                                _selectItem(f);
-                              },
-                            ),
-                      )
-                      .toList())
-                  ..add(new Divider())),
-      ]..addAll(showedElements
-          .map(
-            (e) => new SimpleDialogOption(
-                  key: Key(e.toLongString()),
-                  child: _buildOption(e),
-                  onPressed: () {
-                    _selectItem(e);
+      content: Container( // ref. https://stackoverflow.com/a/56355962
+        width: double.maxFinite,
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                  ListView.builder(itemBuilder: (context, i) {
+                    if (i < (widget?.favoriteElements?.length ?? 0)) {
+                      final it = widget?.favoriteElements[i];
+                      return it != null ? SimpleDialogOption(
+                        child: _buildOption(it),
+                        onPressed: () {
+                          _selectItem(it);
+                        },
+                      ) : Container();
+                    } else if ((widget?.favoriteElements?.length ?? 0) == i) {  /// FIXME: Avoid adding divier if favoriteElements is empty
+                      return Divider();
+                    } else if ((widget?.showedElements?.length ?? 0) == i) {
+                      final it = widget?.showedElements[i];
+                      return it != null ? SimpleDialogOption(
+                        child: _buildOption(it),
+                        onPressed: () {
+                          _selectItem(it);
+                        },
+                      ) : Container();
+                    }
+                    return Contaienr();
                   },
-                ),
-          )
-          .toList()));
+                    shrinkWrap: true,
+                    itemCount: (widget?.favoriteElements?.length ?? 0) + (showedElements?.length ?? 0) + 1,
+                  )
+              )
+            ]
+        ),
+      ),
+    );
+  }
 
   Widget _buildOption(CountryCode e) {
     return Container(
